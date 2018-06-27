15d5d2a2d51b6c38b2ee9b6fbfbdca0efbf57243
Oreki Dawson <dongsongning989@hotmail.com>
2018-06-27 22:44:20 +0800

    6月27日第二次提交。

--- SH000001/Form1.Designer.cs
+++ SH000001/Form1.Designer.cs
@@ -28,8 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -45,23 +59,95 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(68, 227);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 207);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(328, 124);
+            this.label2.Size = new System.Drawing.Size(308, 128);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(90, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(146, 400);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(202, 400);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(258, 400);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(314, 400);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(370, 400);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(934, 509);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Oreki Stock Assistant";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,6 +157,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
 
--- SH000001/Form1.cs
+++ SH000001/Form1.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SH000001
 {
     public partial class Form1 : Form
     {
+        private Point _offset;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,24 +17,110 @@ namespace SH000001
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //try
-            //{
-            WebClient MyWebClient = new WebClient { Credentials = CredentialCache.DefaultCredentials };
-            byte[] pageData = MyWebClient.DownloadData("https://hq.sinajs.cn/?format=text&list=sh000001");
-            string pageHtml = Encoding.Default.GetString(pageData);
-            string stockContent = pageHtml.Split('=')[1];
-            string[] stockContentList = stockContent.Split(',');
-            var YesterdayClose =Convert.ToDouble(stockContentList[2]) ;
-            var PriceNow = Convert.ToDouble(stockContentList[3]) ;
-            //MessageBox.Show(stockContent);
-            var percent = (PriceNow > YesterdayClose) ? (PriceNow - YesterdayClose) / YesterdayClose :
-                (PriceNow == YesterdayClose) ? 0.00 : (PriceNow / YesterdayClose) - 1;
-            label2.Text = (percent*100).ToString();
-            //}
-            //catch (Exception)
-            //{
-            // ignored
-            //}
+            GetStock000001();
+            GetStockColor();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            GetStock000001();
+            GetStockColor();
+        }
+
+        private void GetStock000001()
+        {
+            try
+            {
+                WebClient myWebClient = new WebClient {Credentials = CredentialCache.DefaultCredentials};
+                byte[] pageData = myWebClient.DownloadData("https://hq.sinajs.cn/?format=text&list=sh000001");
+                string pageHtml = Encoding.Default.GetString(pageData);
+                //拆解#0
+                string stockContent = pageHtml.Split('=')[1];
+                //拆解#1
+                string[] stockContentList = stockContent.Split(',');
+                //昨收
+                double yesterdayClose = Convert.ToDouble(stockContentList[2]);
+                //现价
+                double priceNow = Convert.ToDouble(stockContentList[3]);
+                //计算百分比
+                double percent = priceNow > yesterdayClose ? (priceNow - yesterdayClose) / yesterdayClose :
+                    priceNow < yesterdayClose ? priceNow / yesterdayClose - 1 : 0.00;
+                //百分比精确至两位
+                label2.Text = priceNow.ToString("F") + @"      " + (priceNow > yesterdayClose ? "+" : "") +
+                              (percent * 100).ToString("F") + @"%";
+                //红涨绿跌
+                Color redGreen = priceNow > yesterdayClose ? Color.Red :
+                    priceNow < yesterdayClose ? Color.Green : Color.Black;
+                label2.ForeColor = redGreen;
+                pictureBox1.BackColor = redGreen;
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            //获取API数据
+        }
+
+        private void GetStockColor()
+        {
+            try
+            {
+                string[] stockList = {"sz399001", "sz399006", "sh000300", "sh000905", "sh000016"};
+                foreach (string s in stockList)
+                {
+                    WebClient myWebClient = new WebClient {Credentials = CredentialCache.DefaultCredentials};
+                    byte[] pageData = myWebClient.DownloadData("https://hq.sinajs.cn/?format=text&list=" + s);
+                    string pageHtml = Encoding.Default.GetString(pageData);
+                    //拆解#0
+                    string stockContent = pageHtml.Split('=')[1];
+                    //拆解#1
+                    string[] stockContentList = stockContent.Split(',');
+                    //昨收
+                    double yesterdayClose = Convert.ToDouble(stockContentList[2]);
+                    //现价
+                    double priceNow = Convert.ToDouble(stockContentList[3]);
+                    //红涨绿跌
+                    Color redGreen = priceNow > yesterdayClose ? Color.Red :
+                        priceNow < yesterdayClose ? Color.Green : Color.Black;
+                    switch (s)
+                    {
+                        case "sz399001":
+                            pictureBox2.BackColor = redGreen;
+                            break;
+                        case "sz399006":
+                            pictureBox3.BackColor = redGreen;
+                            break;
+                        case "sh000300":
+                            pictureBox4.BackColor = redGreen;
+                            break;
+                        case "sh000905":
+                            pictureBox5.BackColor = redGreen;
+                            break;
+                        case "sh000016":
+                            pictureBox6.BackColor = redGreen;
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Left != e.Button) return;
+            Point cur = PointToScreen(e.Location);
+            _offset = new Point(cur.X - Left, cur.Y - Top);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Left != e.Button) return;
+            Point cur = MousePosition;
+            Location = new Point(cur.X - _offset.X, cur.Y - _offset.Y);
         }
     }
 }
